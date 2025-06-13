f429c9192fbd8faccc5751f38fc96f514822ab78
Taylo <Taylo@MARIEL>
2025-06-13 15:34:45 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// create list of num and display num greater than 80
+
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create list
+        List<int> numbers = new List<int> { 55, 200, 740, 76, 230, 482, 95 };
+
+        Console.WriteLine("The numbers greater than 80 are:");
+
+        // Loop through the list and display numbers greater than 80
+        foreach (int num in numbers)
+        {
+            if (num > 80)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
